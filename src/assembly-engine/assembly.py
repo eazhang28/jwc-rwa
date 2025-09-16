2a792fa7f2ee64225d380d8fec7d9338463e8908
eazboilermaker <109307925+eazboilermaker@users.noreply.github.com>
2025-09-16 16:37:18 -0400

    current iteration

--- src/assembly-engine/assembly.py
+++ src/assembly-engine/assembly.py
@@ -1,9 +1,19 @@
 import numpy as np
+import yaml
 
-class Builder():
-    def __init__(self):
-        self.offset = np.array([0,0])
+class Composer():
+    def __init__(self, paragraph):
+        self.offset: np.array = np.array([0,0])
+        self.paragraph = paragraph
 
-
-    def coord_map(self):
+    def _parse_gcode(self):
         pass
+        
+
+if __name__ == "__main__":
+    with open("config.yaml", 'r') as file:
+        config = yaml.safe_load(file)
+    
+    c = Composer("whats up")
+    print(c.paragraph)
+    # c.compose
