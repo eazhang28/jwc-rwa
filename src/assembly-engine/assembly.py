e1360c65e680787604866c4321cf6d28b17bf48f
Evan zhang <evanzh128@gmail.com>
2025-10-29 23:43:45 -0400

    un(robustly)-tested coordinate class, get composer done

--- src/assembly-engine/assembly.py
+++ src/assembly-engine/assembly.py
@@ -1,45 +1,47 @@
 import numpy as np
 import yaml
-import os
-import regex as re
 from cmap import Character
 from collections import deque
 
-class Composer():
-    '''
-        Only one should exist in run time.
-    '''
+
+class Composer:
+    """
+    Only one should exist in run time.
+    """
+
     def __init__(self, text: str, config):
         self.config = config
-        self.offset: np.array = np.array([0,0])
+        self.offset: np.array = np.array([0, 0])
         self.spacing: float = config["character_spacing"]
-        self.text: str= text
+        self.text: str = text
         self.__char_cache = {}
         self.__get_chars()
-    
+
     def __get_chars(self):
         chars = set(list(self.text))
         for c in chars:
             self.__char_cache[c] = Character(c)
 
     def run(self):
-        '''
-        
+        """
+
         1. Compose the gcode
         2. Send the gcode
-        
-        '''
+
+        """
         compiled_gcode = []
         for c in self.text:
-            ch:Character = self.__char_cache[c]
+            ch: Character = self.__char_cache[c]
             ch.apply_shift(self.offset)
             self.offset[0] += max(ch.coords[0]) + self.spacing
             # if self.offset[0] >=
             compiled_gcode.append(ch.gcode)
 
-        # send gcode to gcode uploader 
+        # send gcode to gcode uploader
+
 
 if __name__ == "__main__":
-    with open("config.yaml", 'r') as file:
+    with open("config.yaml", "r") as file:
         config = yaml.safe_load(file)
         c = Composer("what", config)
+
--- src/assembly-engine/cmap.py
+++ src/assembly-engine/cmap.py
@@ -5,76 +5,77 @@ import regex as re
 from collections import deque
 
 
-class Character():
-    def __init__ (self, char, config):
+class Character:
+    def __init__(self, char, config):
         self.char: str = char
         self.config = config
-        self.gcode: str  = self.__locate_chars(char) 
-        self.coords: np.array  = self.__parse_gcode(self.gcode) 
+        self.gcode: str = self.__locate_chars(char)
+        self.coords: np.array = self.__parse_gcode()
 
-    def __locate_chars(self, c:str):
+    def __locate_chars(self, c: str):
         char_path = os.path.join(self.config["CHAR_DIR"], f"{c}.gcode")
-        pattern = r'[a-zA-Z0-9]'
+        pattern = r"[a-zA-Z0-9]"
         if re.match(pattern, c):
             with open(char_path, "r") as f:
-                char_gcode:str = f.read()
+                char_gcode: str = f.read()
                 return char_gcode
 
-    def __parse_gcode(self, c:str):
-        gcode:str = self.gcode
-        lines = gcode.split('\n')
+    def __parse_gcode(self):
+        gcode: str = self.gcode
+        lines = gcode.split("\n")
         pattern = r"[X][-]?\d+\.?\d+\s[Y][-]?\d+\.?\d+"
         positions = []
         for l in lines:
             if res := re.search(pattern=pattern, string=l):
-                pos_X, pos_Y = res.group().split(' ')
+                pos_X, pos_Y = res.group().split(" ")
                 pos_X, pos_Y = deque(pos_X), deque(pos_Y)
                 pos_X.popleft()
                 pos_Y.popleft()
-                positions.append([float(''.join(list(pos_X))), float(''.join(list(pos_Y)))])
+                positions.append(
+                    [float("".join(list(pos_X))), float("".join(list(pos_Y)))]
+                )
         return np.array(positions)
         print(f"positions\n{np.array(positions) + np.array([1,2])}")
 
     def __translate(self):
-        '''
-        
+        """
+
         Internal method translates between gcode and coordinate mappings
-        
-        '''
+
+        """
         coords: np.array = self.coords
         pattern_x = r"[X][-]?\d+\.?\d+"
         pattern_y = r"[Y][-]?\d+\.?\d+"
         gcode = self.gcode
-        lines = gcode.split('\n')
-        print(lines[0])
+        lines = gcode.split("\n")
         i = 0
+        j = 0
         for l in lines:
-            print(i)
-            print(len(lines))
-            X = "X"+str(coords[i][0])
-            Y = "Y"+str(coords[i][1])
-            l = re.sub(pattern_x, X, l)
-            l = re.sub(pattern_y, Y, l)
-            lines[i] = l + '\n'
-            i += 1
-
-        print(lines[0])
-
-
+            line = l
+            if re.search(pattern=pattern_x, string=l) and re.search(
+                pattern=pattern_y, string=l
+            ):
+                i += 1
+                X = f"X{coords[i - 1][0]:.2f}"
+                Y = f"Y{coords[i - 1][1]:.2f}"
+                l = re.sub(pattern_x, X, l)
+                line = re.sub(pattern_y, Y, l)
+            lines[j] = line
+            j += 1
+        lines = "\n".join(lines)
+        self.gcode = lines
 
     def apply_shift(self, shift):
-        '''
+        """
         Applies a linear shifts to coordinates, then updates the downstream gcode file to correspond
-        '''
-        print(self.coords[0])
+        """
         self.coords += shift
-        print(self.coords[0])
         self.__translate()
+
 
 if __name__ == "__main__":
     with open("config.yaml", "r") as file:
-        config = yaml.safe_load(file) 
+        config = yaml.safe_load(file)
         c = Character("a", config)
-        # print(c.gcode)
         c.apply_shift(1)
-        # print(c.gcode)
+        print(c.gcode)
